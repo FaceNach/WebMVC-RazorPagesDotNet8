0d56fa01b4f5e2f776a52683b2f3bc7016555598
FaceNach <ignacioijg@gmail.com>
2024-04-13 16:27:18 -0300

    First commit of the web proyect

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> ObjCategoryList = _dbContext.Categories.ToList();
+            return View(ObjCategoryList);
+        }
+		public IActionResult Create()
+		{
+            return View();
+		}
+
+		[HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match name");
+            }
+
+            if (ModelState.IsValid)
+            {
+				_dbContext.Categories.Add(obj);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+            return View();
+	    }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? CategoryFromDb = _dbContext.Categories.Find(id);
+			// Others ways find in DB
+			//Category? CategoryFromDb1 = _dbContext.Categories.FirstOrDefault(u => u.Id == id); 
+			//Category? CategoryFromDb2 = _dbContext.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+			if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Update(obj);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? CategoryFromDb = _dbContext.Categories.Find(id);
+			// Others ways find in DB
+			//Category? CategoryFromDb1 = _dbContext.Categories.FirstOrDefault(u => u.Id == id); 
+			//Category? CategoryFromDb2 = _dbContext.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+			if (CategoryFromDb == null)
+			{
+				return NotFound();
+			}
+
+			return View(CategoryFromDb);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _dbContext.Categories.Find(id);
+
+			if(obj == null)
+			{
+				return NotFound();
+			}
+			_dbContext.Categories.Remove(obj);
+			_dbContext.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ BulkyWeb/Migrations/20240411193500_UpdatedTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoriesMyProperty",
+                table: "CategoriesMyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "CategoriesMyProperty",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "CategoriesMyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoriesMyProperty",
+                table: "CategoriesMyProperty",
+                column: "Id");
+        }
+    }
+}
